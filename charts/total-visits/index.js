892a0ba5f4590df3ecacc8dc3f23f499c8295496
Brian Weinreich <be.weinreich@gmail.com>
2017-04-04 21:41:32 -0400

    latest

--- charts/total-visits/index.js
+++ charts/total-visits/index.js
@@ -1,5 +1,58 @@
 import './styles.scss';
+import c3 from 'c3';
 
-export default function totalVisits(elem, props={}) {
-  elem.innerHTML = `Hello ${props.name || 'World'}! I'm a super-basic chart!`;
+export default elem => (props={}) => {
+  const dates = props.dates;
+  const totalVisits = props.totalVisits;
+
+  let datesWithHeader = ['Date'].concat(dates);
+  let totalVisitsWithHeader = ['Total Visits'].concat(totalVisits);
+
+  let chart = c3.generate({
+    bindto: '#totalvisitschart',
+    tooltip: {
+      show: false
+    },
+    data: {
+      x: 'Date',
+      y: 'Total Visits',
+      colors: {
+        'Total Visits': '#469AFD',
+      },
+      columns: [
+        datesWithHeader,
+        totalVisitsWithHeader,
+      ],
+      type: 'bar',
+      labels: true
+    },
+    legend: {
+      hide: true
+    },
+    axis: {
+      y: {
+        show: false
+      },
+      x: {
+        type: 'timeseries',
+        label: {
+          show: false
+        },
+        height: 50,
+        tick: {
+          format: '%-m/%-d',
+          outer: false
+        }
+      }
+    },
+    bar: {
+      width: {
+        ratio: 0.9
+      }
+    }
+  });
+
+  elem.innerHTML = `<div id="totalvisitschart"></div>`;
+
+  
 }
--- charts/total-visits/story.js
+++ charts/total-visits/story.js
@@ -1,15 +1,21 @@
 import * as React from 'react';
 import {storiesOf, action} from '@kadira/storybook';
 import {chartAsReactComponent} from '../index';
+import moment from 'moment';
+// import {} from 'moment-range';
 
 import totalVisits from './index';
 
 const TotalVisits = chartAsReactComponent(totalVisits);
 
+// var dates1 = moment.range(moment().subtract(7, 'd'), Date.now()).toArray('days').map(date => date.format('YYYY-MM-DD'))
+var dates1 = [moment().subtract(7, 'd').format("YYYY-MM-DD"), moment().format("YYYY-MM-DD")]
+var totalVisits1 = [100,50]
+
 storiesOf('Total Visits', module)
-  .add('With no name', () => (
-    <TotalVisits />
+  .add('Last 7 days of visits', () => (
+    <TotalVisits dates={dates1} totalVisits={totalVisits1} />
   ))
-  .add('With name', () => (
-    <TotalVisits name="Bob" />
+  .add('Last 20 days of visits', () => (
+    <TotalVisits dates={dates1} totalVisits={totalVisits1} />
   ))
