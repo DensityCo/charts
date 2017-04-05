83e5dfd989e386a6f72e93b082722581ab446487
Brian Weinreich <be.weinreich@gmail.com>
2017-04-04 22:18:41 -0400

    total visits chart

--- charts/total-visits/index.js
+++ charts/total-visits/index.js
@@ -1,58 +1,62 @@
 import './styles.scss';
 import c3 from 'c3';
 
-export default elem => (props={}) => {
-  const dates = props.dates;
-  const totalVisits = props.totalVisits;
+export default function totalVisitsChart(elem) {
+  const div = document.createElement('div');
+  elem.appendChild(div);
 
-  let datesWithHeader = ['Date'].concat(dates);
-  let totalVisitsWithHeader = ['Total Visits'].concat(totalVisits);
+  return (props={}) => {
+    const dates = props.dates;
+    const totalVisits = props.totalVisits;
+    let datesWithHeader = ['Date'].concat(dates);
+    let totalVisitsWithHeader = ['Total Visits'].concat(totalVisits);
 
-  let chart = c3.generate({
-    bindto: '#totalvisitschart',
-    tooltip: {
-      show: false
-    },
-    data: {
-      x: 'Date',
-      y: 'Total Visits',
-      colors: {
-        'Total Visits': '#469AFD',
-      },
-      columns: [
-        datesWithHeader,
-        totalVisitsWithHeader,
-      ],
-      type: 'bar',
-      labels: true
-    },
-    legend: {
-      hide: true
-    },
-    axis: {
-      y: {
+    div.innerHTML = `<div id="totalvisitschart"></div>`;
+
+    c3.generate({
+      bindto: '#totalvisitschart',
+      tooltip: {
         show: false
       },
-      x: {
-        type: 'timeseries',
-        label: {
+      data: {
+        x: 'Date',
+        y: 'Total Visits',
+        colors: {
+          'Total Visits': '#469AFD',
+        },
+        columns: [
+          datesWithHeader,
+          totalVisitsWithHeader,
+        ],
+        type: 'bar',
+        labels: true
+      },
+      legend: {
+        hide: true
+      },
+      axis: {
+        y: {
           show: false
         },
-        height: 50,
-        tick: {
-          format: '%-m/%-d',
-          outer: false
+        x: {
+          type: 'timeseries',
+          label: {
+            show: false
+          },
+          height: 50,
+          tick: {
+            format: '%-m/%-d',
+            outer: false
+          }
+        }
+      },
+      bar: {
+        width: {
+          ratio: 0.9
         }
       }
-    },
-    bar: {
-      width: {
-        ratio: 0.9
-      }
-    }
-  });
+    });
 
-  elem.innerHTML = `<div id="totalvisitschart"></div>`;
+  }
 
-  
 }
--- charts/total-visits/story.js
+++ charts/total-visits/story.js
@@ -2,20 +2,34 @@ import * as React from 'react';
 import {storiesOf, action} from '@kadira/storybook';
 import {chartAsReactComponent} from '../index';
 import moment from 'moment';
-// import {} from 'moment-range';
 
 import totalVisits from './index';
 
 const TotalVisits = chartAsReactComponent(totalVisits);
 
-// var dates1 = moment.range(moment().subtract(7, 'd'), Date.now()).toArray('days').map(date => date.format('YYYY-MM-DD'))
-var dates1 = [moment().subtract(7, 'd').format("YYYY-MM-DD"), moment().format("YYYY-MM-DD")]
-var totalVisits1 = [100,50]
+// Generate some random data...
+var createData = (duration) => {
+  var dates = [];
+  var visits = [];
+  
+  [...Array(duration).keys()].map((day) => {
+    dates.push(moment().subtract(day, 'd').format("YYYY-MM-DD"));
+    visits.push(Math.round(Math.random() * (100 - 0)));
+  });
+
+  return {
+    'dates': dates,
+    'visits': visits
+  }
+}
+
+var data1 = createData(7);
+var data2 = createData(20);
 
 storiesOf('Total Visits', module)
   .add('Last 7 days of visits', () => (
-    <TotalVisits dates={dates1} totalVisits={totalVisits1} />
+    <TotalVisits dates={data1['dates']} totalVisits={data1['visits']} />
   ))
   .add('Last 20 days of visits', () => (
-    <TotalVisits dates={dates1} totalVisits={totalVisits1} />
+    <TotalVisits dates={data2['dates']} totalVisits={data2['visits']} />
   ))
