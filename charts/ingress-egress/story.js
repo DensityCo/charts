604f5da6246f1e7a570734f3de2c874a5085f76b
Ryan Gaus <rsg1egoman@gmail.com>
2017-04-28 16:30:20 -0400

    code: Redesign ingress-egress chart's internals
    - Using d3 now.
    - Using a new algorithm for placing markers up and down:
      - let A = a marker
      - let B = the previous marker in the chart that goes in the same direction
      - if A's x position intersect's B's x position, then A should stack above B.
    - Changed `count_change` to `countChange`. This means we'll need another
    major version bump. I think we should be using camelcased stuff even if
    the server doens't return data like that.

--- charts/ingress-egress/story.js
+++ charts/ingress-egress/story.js
@@ -15,7 +15,7 @@ function generateRandomCountChangeEvents() {
   const acc = [];
   for (let i = 0; i < Math.floor(Math.random() * 32); i++) {
     acc.push({
-      count_change: Math.random() > 0.5 ? 1 : -1, // Pick a random direction
+      countChange: Math.random() > 0.5 ? 1 : -1, // Pick a random direction
       timestamp: generateRandomMillisecondPosition(), // and timestamp
     });
   }
@@ -26,9 +26,22 @@ storiesOf('Ingress Egress Chart', module)
   .add('With a few set datapoints', () => (
     <IngressEgress
       events={[
-        {count_change: 1, timestamp: new Date().getTime() - 500000},
-        {count_change: 1, timestamp: new Date().getTime() -  425000 },
-        {count_change: -1, timestamp: new Date().getTime() - 400000 },
+        {countChange: 1, timestamp: new Date().getTime() - 500000},
+        {countChange: 1, timestamp: new Date().getTime() -  425000 },
+        {countChange: -1, timestamp: new Date().getTime() - 420000 },
+        {countChange: 1, timestamp: new Date().getTime() - 400000 },
+      ]}
+    />
+  ))
+  .add('With a few set datapoints that should stack', () => (
+    <IngressEgress
+      events={[
+        {countChange: 1, timestamp: new Date().getTime() - 500000},
+        {countChange: 1, timestamp: new Date().getTime() -  425000 },
+        {countChange: 1, timestamp: new Date().getTime() - 420000 },
+        {countChange: 1, timestamp: new Date().getTime() - 300000 },
+        {countChange: -1, timestamp: new Date().getTime() - 200000 },
+        {countChange: -1, timestamp: new Date().getTime() - 205000 },
       ]}
     />
   ))
@@ -38,9 +51,10 @@ storiesOf('Ingress Egress Chart', module)
   .add('With a shorter duration (ie, 1 min)', () => (
     <IngressEgress
       events={[
-        {count_change: 1, timestamp: new Date().getTime() - 10000},
-        {count_change: -1, timestamp: new Date().getTime() - 7500 },
-        {count_change: 1, timestamp: new Date().getTime() -  50000 },
+        {countChange: -1, timestamp: new Date().getTime() - 5000},
+        {countChange: 1, timestamp: new Date().getTime() - 10000},
+        {countChange: -1, timestamp: new Date().getTime() - 7500 },
+        {countChange: 1, timestamp: new Date().getTime() -  50000 },
       ]}
       graphDurationInMin={1}
     />
