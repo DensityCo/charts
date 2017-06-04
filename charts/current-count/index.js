1fbd521b7e1b8c187064a3144b2a71c9db617389
Gus Cost <guscost@gmail.com>
2017-06-04 12:54:57 -0400

    code: resizable current count

--- charts/current-count/index.js
+++ charts/current-count/index.js
@@ -8,15 +8,15 @@ export default elem => ({label, currentCount, capacity, lastEvent}) => {
   const lastEventDelta = lastEvent ? `Last Event: ${moment(lastEvent).fromNow()}` : '';
 
   ReactDOM.render(<div className="card chart-current-count">
+    <div className="chart-current-count-text">
+      <span>{currentCount}</span>
+    </div>
     <div className="card-body chart-current-count-body">
       <div className="chart-current-count-header">
         <strong>{label}</strong>
         <span style={{flex: '1 1 0%'}}></span>
         <span className="chart-current-count-last-event">{lastEventDelta}</span>
       </div>
-
-      <div className="chart-current-count-text">{currentCount}</div>
-
       <div className="chart-current-count-footer">
         <span>Capacity: {capacity || 'N/A'}</span>
         <div className="chart-current-count-progress-bar">
