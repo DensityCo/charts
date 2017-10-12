073c5e82c76ef476f9574688a14dc457380e891e
Ryan Gaus <rsg1egoman@gmail.com>
2017-10-12 08:54:07 -0400

    code: write a chart helper that provides auto-updating
    Some charts need to be continuously rerendered, even when the props
    don't change. An example would be the real time chart. Previously, we
    just repeatedly rerendered the chart over and over in a setInterval, but
    that was a bit uneven and produced some animation artifacts.

--- src/charts/index.js
+++ src/charts/index.js
@@ -16,4 +16,38 @@ function chartAsReactComponent(chart) {
   }
 }
 
-module.exports = {chartAsReactComponent};
+function chartAsReactComponentAutoUpdate(chart) {
+  return class extends React.Component {
+    componentDidMount() {
+      const animate = frame => {
+        if (window.requestAnimationFrame) {
+          return window.requestAnimationFrame(frame);
+        } else {
+          return setInterval(frame, 1000 / 30); // 30 FPS
+        }
+      };
+
+      this.animationFrame = animate(() => {
+        if (this.updateChart) {
+          this.updateChart(this.props);
+        }
+      });
+    }
+    componentWillUnmount() {
+      const unanimate = window.cancelAnimationFrame || window.clearInterval;
+      unanimate(this.animationFrame);
+    }
+    render() {
+      return <div ref={ref => {
+        if (ref !== null) {
+          if (!this.updateChart) {
+            this.updateChart = chart(ref);
+          }
+          this.updateChart(this.props);
+        }
+      }} />;
+    }
+  }
+}
+
+module.exports = {chartAsReactComponent, chartAsReactComponentAutoUpdate};
