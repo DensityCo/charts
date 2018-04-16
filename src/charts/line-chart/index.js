42f5760cc769645ced80795ee69404e6d58b47c5
Ryan Gaus <rsg1egoman@gmail.com>
2018-04-16 17:33:09 -0400

    code: implmented waterline data rendering

--- src/charts/line-chart/index.js
+++ src/charts/line-chart/index.js
@@ -7,12 +7,30 @@ const ONE_MINUTE_IN_MS = 60 * 1000,
       ONE_HOUR_IN_MS = ONE_MINUTE_IN_MS * 60,
       ONE_DAY_IN_MS = ONE_HOUR_IN_MS * 60;
 
+const colorVariables = {
+  "grayCinder": "#222A2E",
+
+  "grayDarkest": "#4E5457",
+  "grayDarker": "#8E9299",
+  "grayDark": "#B4B8BF",
+  "gray": "#CBCFD6",
+  "grayLight": "#E8E8ED",
+  "grayLighter": "#F0F0F2",
+  "grayLightest": "#FAFAFA",
+
+  "brandPrimary": "#4198FF",
+  "brandSuccess": "#80CD80",
+  "brandDanger": "#FF5454",
+  "brandWarning": "#FFBA08",
+};
+
 
 export default function lineChart(elem, props={}) {
   const svg = d3.select(elem).append('svg');
 
   const layers = {
     background: svg.append('g').attr('class', 'layer layer-bg'),
+    data: svg.append('g').attr('class', 'layer layer-data'),
 
     xAxis: svg.append('g').attr('class', 'layer layer-x-axis'),
     yAxis: svg.append('g').attr('class', 'layer layer-y-axis'),
@@ -22,9 +40,15 @@ export default function lineChart(elem, props={}) {
     timeZone,
     data,
 
-    start,
-    end,
+    xAxis,
+    yAxis,
+
+    xAxisStart,
+    xAxisEnd,
+    yAxisStart,
+    yAxisEnd,
   }) => {
+
     // ------------------------------------------------------------------------
     // ADJUST GRAPH SIZE
     // ------------------------------------------------------------------------
@@ -34,7 +58,7 @@ export default function lineChart(elem, props={}) {
     const topMargin = 20;
     const leftMargin = 56;
     const rightMargin = 20;
-    const bottomMargin = 20;
+    const bottomMargin = 56;
 
     const graphWidth = svgWidth - leftMargin - rightMargin;
     const graphHeight = svgHeight - topMargin - bottomMargin;
@@ -44,28 +68,34 @@ export default function lineChart(elem, props={}) {
 
     layers.xAxis.attr('transform', `translate(${leftMargin},${topMargin+graphHeight})`)
     layers.yAxis.attr('transform', `translate(${leftMargin},${topMargin})`)
+    layers.data.attr('transform', `translate(${leftMargin},${topMargin})`)
 
     // ------------------------------------------------------------------------
     // GATHERING DATA POINTS
     // ------------------------------------------------------------------------
     const dataPoints = {};
 
-    dataPoints.firstEvent = data[0];
-    dataPoints.lastEvent = data.slice(-1)[0];
+    const defaultDataset = data.find(i => i.name === 'default');
+
+    dataPoints.firstEvent = defaultDataset.data[0];
+    dataPoints.lastEvent = defaultDataset.data.slice(-1)[0];
 
     dataPoints.firstEventXValue = moment.utc(dataPoints.firstEvent.timestamp).valueOf(); /* epoch ms */
     dataPoints.firstEventYValue = dataPoints.firstEvent.value;
     dataPoints.lastEventXValue = moment.utc(dataPoints.lastEvent.timestamp).valueOf(); /* epoch ms */
     dataPoints.lastEventYValue = dataPoints.lastEvent.value;
 
-    dataPoints.startXValue = start || dataPoints.firstEventXValue;
-    dataPoints.endXValue = end || dataPoints.lastEventXValue;
+    dataPoints.eventWithSmallestValue = defaultDataset.data.reduce((a, b) => a.value < b.value ? a : b, {value: Infinity});
+    dataPoints.eventWithLargestValue = defaultDataset.data.reduce((a, b) => a.value > b.value ? a : b, {value: -1});
 
-    dataPoints.startYValue = start || dataPoints.firstEventYValue;
-    dataPoints.endYValue = end || dataPoints.lastEventYValue;
+    dataPoints.startXValue = xAxisStart ? moment.utc(xAxisStart).valueOf() : dataPoints.firstEventXValue;
+    dataPoints.endXValue = xAxisEnd ? moment.utc(xAxisEnd).valueOf() : dataPoints.lastEventXValue;
+
+    dataPoints.startYValue = typeof yAxisStart !== 'undefined' ? yAxisStart : dataPoints.eventWithSmallestValue.value;
+    dataPoints.endYValue = typeof yAxisEnd !== 'undefined' ? yAxisEnd : dataPoints.eventWithLargestValue.value;
 
     const xScale = d3.scaleLinear()
-      .rangeRound([graphWidth, 0])
+      .rangeRound([0, graphWidth])
       .domain([dataPoints.startXValue, dataPoints.endXValue]);
 
     const yScale = d3.scaleLinear()
@@ -75,28 +105,19 @@ export default function lineChart(elem, props={}) {
     // ------------------------------------------------------------------------
     // X AXIS
     // ------------------------------------------------------------------------
-    const xAxis = xAxisDailyTick({
-      timeBetweenTicksInMs: 1 * ONE_MINUTE_IN_MS,
-    });
-
     xAxis({
       scale: xScale,
       timeZone,
+      bottomMargin,
     }, layers.xAxis);
 
     // ------------------------------------------------------------------------
     // Y AXIS
     // ------------------------------------------------------------------------
-    const yAxis = yAxisMinMax({
-      leftOffset: 20,
-      axisRuleLineDashWidth: 4,
-      axisRuleLineDashSpacing: 10,
-    });
-
     yAxis({
       scale: yScale,
-      firstEventYValue: dataPoints.firstEventYValue,
-      lastEventYValue: dataPoints.lastEventYValue,
+      firstEventYValue: dataPoints.startYValue,
+      lastEventYValue: dataPoints.endYValue,
       graphWidth,
       leftMargin,
     }, layers.yAxis);
@@ -112,27 +133,67 @@ export default function lineChart(elem, props={}) {
       .attr('y', d => topMargin)
       .attr('width', graphWidth)
       .attr('height', graphHeight)
-      .attr('fill', 'red')
+      .attr('fill', '#fff')
     backgroundSelection.exit().remove()
+
+    // ------------------------------------------------------------------------
+    // WATERLINES
+    // ------------------------------------------------------------------------
+    const dataSelection = layers.data.selectAll('.data').data(data);
+    dataSelection.enter()
+      .append('g')
+        .attr('class', 'data')
+    .merge(dataSelection)
+      .each(function(d) {
+        return d.type(d)({
+          xScale,
+          yScale,
+          graphHeight,
+          graphWidth,
+          dataPoints,
+        }, this); /* this = reference to the g.data created above */
+      });
   }
 }
 
-export function xAxisDailyTick({formatter, timeBetweenTicksInMs}) {
-  return ({timeZone, scale}, element) => {
-    element.call(
-      d3.axisBottom(scale)
-        // Position a tick at the first whole hour on the axis, then another tick for each hour
-        // thereafter.
-        .tickSizeOuter(0)
-        .tickFormat(formatter || (n => {
-          // "5a" or "8p"
-          const timeFormat = moment.utc(n).tz(timeZone).format(`hA`);
-          return timeFormat.slice(
-            0, 
-            timeFormat.startsWith('12') ? -1 : -2
-          ).toLowerCase();
-        }))
-    );
+export function xAxisDailyTick({
+  formatter,
+  tickValues,
+  bottomOffset, /* distance between the bottom edge of the svg and the axis labels */
+}) {
+  return ({timeZone, scale, bottomMargin}, element) => {
+    const axisFontSize = 14;
+
+    // Use the proper font size for the axis
+    element.attr('font-size', axisFontSize)
+
+    let axis = d3.axisBottom(scale)
+      // Position a tick at the first whole hour on the axis, then another tick for each hour
+      // thereafter.
+      .tickFormat(formatter || (n => {
+        // "5a" or "8p"
+        const timeFormat = moment.utc(n).tz(timeZone).format(`hA`);
+        return timeFormat.slice(
+          0, 
+          timeFormat.startsWith('12') ? -1 : -2
+        ).toLowerCase();
+      }));
+
+    // If tick values were passed, then use those values on the axis
+    if (tickValues) {
+      axis = axis.tickValues(tickValues);
+    }
+
+    // Render the axis
+    element.call(axis);
+
+    // Remove some parts of the axis that aren't needed
+    element.select('.domain').remove();
+    element.selectAll('.tick line').remove();
+
+    // Adjust the vertical spacing of each tick on the axis
+    element.selectAll('.tick text')
+      .attr('transform', `translate(0,${-10 + bottomMargin - bottomOffset})`)
   };
 }
 
@@ -140,18 +201,20 @@ export function yAxisMinMax({
   formatter,
   leftOffset, /* distance between the left edge of the svg and the axis labels */
 
+  points,
+
   axisRuleLineDashWidth,
   axisRuleLineDashSpacing,
 }) {
   return ({graphWidth, leftMargin, firstEventYValue, lastEventYValue, scale}, element) => {
     const axisFontSize = 14;
-    const points = [
-      firstEventYValue,
-      6,
-      lastEventYValue,
+    const axisPoints = [
+      {value: firstEventYValue, hasRule: false},
+      ...(points || []),
+      {value: lastEventYValue, hasRule: false},
     ];
 
-    const selection = element.selectAll('.axis-y-point').data(points);
+    const selection = element.selectAll('.axis-y-point').data(axisPoints);
 
     // On enter selection, create a group with a text element and line inside. These will be used to
     // render each datapoint on the y axis.
@@ -159,9 +222,16 @@ export function yAxisMinMax({
     const enterSelectionGroup = enterSelection.append('g')
       .attr('class', 'axis-y-point')
       .attr('font-size', axisFontSize)
+    enterSelectionGroup.append('text')
+      .attr('class', 'axis-y-point-label')
+    enterSelectionGroup.append('path')
+      .attr('class', 'axis-y-point-rule')
+
+    // On merge, add the text to each datapoint on the axis.
+    const mergeSelection = enterSelectionGroup.merge(enterSelection)
       .attr('transform', d => {
         let x = -1 * (leftMargin - leftOffset);
-        let y = scale(d);
+        let y = scale(d.value);
 
         // For all datapoints except for the first one, draw them a bit higher so the label is
         // centered about the datapoint (instead, the datapoint would be at the bottom of the point)
@@ -170,19 +240,18 @@ export function yAxisMinMax({
         }
         return `translate(${x},${y})`;
       });
-    enterSelectionGroup.append('text')
-      .attr('class', 'axis-y-point-label')
+    mergeSelection.select('.axis-y-point-label')
       .text(formatter || (d => {
-        return `${d}`;
+        return `${d.value}`;
       }));
-    enterSelectionGroup.append('path')
-      .attr('class', 'axis-y-point-rule')
+    mergeSelection.select('.axis-y-point-rule')
       .attr('transform', `translate(${leftMargin - leftOffset},${-1 * (axisFontSize * 0.4)})`)
-      .attr('stroke', 'green')
+      .attr('stroke', colorVariables.gray)
       .attr('stroke-width', '1')
+      .attr('shape-rendering', 'crispEdges')
       .attr('d', d => {
-        // Don't add horizontal lines for the labels at the extremes
-        if (d === firstEventYValue || d === lastEventYValue) {
+        // Don't add horizontal lines for points that don't specify a rule is required.
+        if (!d.hasRule) {
           return '';
         }
 
@@ -195,13 +264,55 @@ export function yAxisMinMax({
         })(graphWidth)}`;
       });
 
-    // On merge, add the text to each datapoint on the axis.
-    const mergeSelection = selection.merge(enterSelection);
-    // mergeSelection
-
     // On exit, remove any axis points that are no longer required
     selection.exit().remove();
 
     return element;
+  };
+}
+
+export function dataWaterline({
+  data,
+  color,
+  borderColor,
+}) {
+  return ({xScale, yScale, graphHeight, dataPoints}, element) => {
+    const waterlinePrefix = `M0,${graphHeight}` + /* move to the lower left of the graph */
+      `L0,${yScale(dataPoints.startYValue)}`; /* move to the first datapoint in the waterline */
+    const waterlinePostfix = `H${xScale(dataPoints.lastEventXValue)}V${graphHeight}H1`;
+
+    const waterlineSelection = d3.select(element).selectAll('.waterline').data([data]);
+    const waterlineSelectionGroup = waterlineSelection.enter()
+      .append('g')
+        .attr('class', 'waterline');
+    waterlineSelectionGroup.append('path')
+      .attr('class', 'waterline-fill')
+      .attr('shape-rendering', 'crispEdges')
+      .attr('fill', color);
+    waterlineSelectionGroup.append('path')
+      .attr('class', 'waterline-stroke')
+      .attr('stroke', borderColor)
+      .attr('shape-rendering', 'crispEdges')
+      .attr('fill', 'transparent');
+
+    waterlineSelectionGroup.merge(waterlineSelection)
+      .select('.waterline-fill')
+      .attr('d', d => {
+        return waterlinePrefix + d.reduce((acc, i) => {
+          const xPosition = xScale(moment.utc(i.timestamp).valueOf());
+          const yPosition = yScale(i.value);
+          return `${acc} H${xPosition} V${yPosition}`;
+        }, '') + waterlinePostfix;
+      });
+    waterlineSelectionGroup.merge(waterlineSelection)
+      .select('.waterline-stroke')
+      .attr('d', d => {
+        return `M0,${graphHeight} M0,${yScale(dataPoints.firstEventYValue)}` + d.reduce((acc, i) => {
+          const xPosition = xScale(moment.utc(i.timestamp).valueOf());
+          const yPosition = yScale(i.value);
+          return `${acc} H${xPosition} V${yPosition}`;
+        }, '');
+      });
+    waterlineSelection.exit().remove();
   };
 }
--- src/charts/line-chart/story.js
+++ src/charts/line-chart/story.js
@@ -2,18 +2,65 @@ import * as React from 'react';
 import {storiesOf} from '@storybook/react';
 import {chartAsReactComponent} from '../index';
 
-import lineChart from './index';
+import lineChart, {
+  xAxisDailyTick,
+  yAxisMinMax,
+  dataWaterline,
+} from './index';
+
+const ONE_MINUTE_IN_MS = 60 * 1000,
+      ONE_HOUR_IN_MS = ONE_MINUTE_IN_MS * 60,
+      ONE_DAY_IN_MS = ONE_HOUR_IN_MS * 60;
 
 const LineChart = chartAsReactComponent(lineChart);
 
 storiesOf('Line Chart', module)
   .add('With no name', () => (
     <LineChart
-      timeZone="America/New_York"
+      timeZone="UTC"
+
+      xAxis={xAxisDailyTick({
+        timeBetweenTicksInMs: 1 * ONE_MINUTE_IN_MS,
+        bottomOffset: 40,
+      })}
+
+      yAxis={yAxisMinMax({
+        leftOffset: 20,
+        axisRuleLineDashWidth: 4,
+        axisRuleLineDashSpacing: 10,
+        points: [
+          {value: 20, hasRule: true},
+          {value: 10, hasRule: false},
+        ],
+      })}
+      yAxisEnd={40}
+      yAxisStart={0}
+
       data={[
-        {value: 5, timestamp: '2018-04-16T14:00:00.000Z'},
-        {value: 6, timestamp: '2018-04-16T20:00:00.000Z'},
-        {value: 10, timestamp: '2018-04-16T23:00:00.000Z'},
+        {
+          name: 'default',
+          type: dataWaterline,
+          color: 'rgba(65, 152, 255, 0.2)',
+          borderColor: 'rgb(65, 152, 255)',
+          data: [
+            {value: 18, timestamp: '2018-04-16T14:00:00.000Z'},
+            {value: 7, timestamp: '2018-04-16T20:00:00.000Z'},
+            {value: 8, timestamp: '2018-04-16T21:00:00.000Z'},
+            {value: 10, timestamp: '2018-04-16T23:00:00.000Z'},
+          ],
+        },
+        // {
+        //   name: 'secondary',
+        //   type: dataWaterline,
+        //   color: 'rgba(255, 0, 0, 0.2)',
+        //   borderColor: 'red',
+        //   data: [
+        //     {value: 18, timestamp: '2018-04-16T14:00:00.000Z'},
+        //     {value: 7, timestamp: '2018-04-16T20:00:00.000Z'},
+        //     {value: 20, timestamp: '2018-04-16T21:00:00.000Z'},
+        //     {value: 20, timestamp: '2018-04-16T23:00:00.000Z'},
+        //   ],
+        // },
       ]}
     />
   ))
